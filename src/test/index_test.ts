570a6287d86118b17197e1d33cceaf486f171341
Justin Fagnani <justinfagnani@google.com>
2019-06-04 15:10:08 -0700

    Add Prettier formatting

--- src/test/index_test.ts
+++ src/test/index_test.ts
@@ -20,58 +20,79 @@ import * as path from 'path';
 import * as fs from 'fs';
 
 const stripIndentTag = (strings: TemplateStringsArray, ...values: any[]) => {
-  const result = values.reduce((acc, v, i) => acc + String(v) + strings[i+1], strings[0]);
+  const result = values.reduce(
+    (acc, v, i) => acc + String(v) + strings[i + 1],
+    strings[0]
+  );
   return stripIndent(result).trim();
 };
 const js = stripIndentTag;
 const soy = stripIndentTag;
 
 suite('grimlock', () => {
-
   suite('lit-html', () => {
-
     suite('template function declaration', () => {
-
       test('simple declaration', () => {
-        assert.equal(convertModule('test.ts', js`
+        assert.equal(
+          convertModule(
+            'test.ts',
+            js`
           import {html} from 'lit-html';
 
           /**
            * @soyCompatible
            */
           export const t = () => html\`<div></div>\`;
-        `).output, soy`
+        `
+          ).output,
+          soy`
           {namespace test.ts}
 
           {template .t}
           <div></div>
           {/template}
-        `);
+        `
+        );
       });
 
       test('missing @soyCompatible', () => {
         // Documenting current behavior. Perhaps we shold error with
         // "nothing to translate"
-        assert.equal(convertModule('test.ts', js`
+        assert.equal(
+          convertModule(
+            'test.ts',
+            js`
           import {html} from 'lit-html';
-        `).output, soy`
+        `
+          ).output,
+          soy`
           {namespace test.ts}
-        `);
+        `
+        );
       });
 
       test('incorrect html tag', () => {
-        const result = convertModule('test.ts', js`
+        const result = convertModule(
+          'test.ts',
+          js`
           /**
            * @soyCompatible
            */
           export const t = () => html\`<div></div>\`;
-          `);
+          `
+        );
         assert.equal(result.diagnostics.length, 1);
-        assert.include(result.diagnostics[0].message, 'template tags must be named imports');
+        assert.include(
+          result.diagnostics[0].message,
+          'template tags must be named imports'
+        );
       });
 
       test('parameters and expression', () => {
-        assert.equal(convertModule('test.ts', js`
+        assert.equal(
+          convertModule(
+            'test.ts',
+            js`
           import {html} from 'lit-html';
 
           /**
@@ -79,7 +100,9 @@ suite('grimlock', () => {
            */
           export const t = (a: string, b: number, c: boolean) => 
               html\`<div>\${a}\${b}\${c}</div>\`;
-        `).output, soy`
+        `
+          ).output,
+          soy`
           {namespace test.ts}
 
           {template .t}
@@ -88,11 +111,14 @@ suite('grimlock', () => {
             {@param c: bool}
           <div>{$a}{$b}{$c}</div>
           {/template}
-        `);
+        `
+        );
       });
 
       test('error on unsupported statements', () => {
-        const result = convertModule('test.ts', js`
+        const result = convertModule(
+          'test.ts',
+          js`
           import {html} from 'lit-html';
 
           /**
@@ -104,18 +130,23 @@ suite('grimlock', () => {
              */
             const inner = () => html\`\${a}\`;
           };
-        `);
+        `
+        );
         assert.equal(result.diagnostics.length, 2);
         assert.include(result.diagnostics[0].message, 'unsupported statement');
-        assert.include(result.diagnostics[1].message, 'must return a TemplateResult');
+        assert.include(
+          result.diagnostics[1].message,
+          'must return a TemplateResult'
+        );
       });
-
     });
 
     suite('expressions', () => {
-
       test('subtemplate call', () => {
-        assert.equal(convertModule('test.ts', js`
+        assert.equal(
+          convertModule(
+            'test.ts',
+            js`
           import {html} from 'lit-html';
 
           /**
@@ -127,7 +158,9 @@ suite('grimlock', () => {
            * @soyCompatible
            */
           export const t1 = () => html\`<div></div>\`;
-        `).output, soy`
+        `
+          ).output,
+          soy`
           {namespace test.ts}
 
           {template .t2}
@@ -137,43 +170,65 @@ suite('grimlock', () => {
           {template .t1}
           <div></div>
           {/template}
-        `);
+        `
+        );
       });
 
       test('error on unknown reference', () => {
-        const result = convertModule('test.ts', js`
+        const result = convertModule(
+          'test.ts',
+          js`
           import {html} from 'lit-html';
 
           /**
            * @soyCompatible
            */
           export const t = () => html\`\${a}\`;
-        `);
+        `
+        );
         assert.equal(result.diagnostics.length, 1);
         assert.include(result.diagnostics[0].message, 'unknown identifier');
       });
 
       test('references to parameters', () => {
-        const result = convertModule('test.ts', js`
+        const result = convertModule(
+          'test.ts',
+          js`
           import {html} from 'lit-html';
 
           /**
            * @soyCompatible
            */
           export const t = (a: string) => html\`\${a}\`;
-        `);
-        assert.equal(result.output, soy`
+        `
+        );
+        assert.equal(
+          result.output,
+          soy`
           {namespace test.ts}
 
           {template .t}
             {@param a: string}
           {$a}
           {/template}
-        `);
+        `
+        );
         assert.equal(result.diagnostics.length, 0);
       });
 
-      const binaryOps = ['+', '-', '*', '/', '%', '<', '>', '>=', '<=', ['||', ' or '], ['&&', ' and ']];
+      const binaryOps = [
+        '+',
+        '-',
+        '*',
+        '/',
+        '%',
+        '<',
+        '>',
+        '>=',
+        '<=',
+        ['||', ' or '],
+        ['&&', ' and '],
+      ];
       for (let op of binaryOps) {
         let expected = op;
         if (Array.isArray(op)) {
@@ -181,15 +236,20 @@ suite('grimlock', () => {
           op = op[0];
         }
         test(`binary ${op} operator`, () => {
-          const result = convertModule('test.ts', js`
+          const result = convertModule(
+            'test.ts',
+            js`
             import {html} from 'lit-html';
 
             /**
              * @soyCompatible
              */
             export const t = (a: string, b: string) => html\`\${a ${op} b}\`;
-          `);
-          assert.equal(result.output, soy`
+          `
+          );
+          assert.equal(
+            result.output,
+            soy`
             {namespace test.ts}
 
             {template .t}
@@ -197,83 +257,99 @@ suite('grimlock', () => {
               {@param b: string}
             {$a${expected}$b}
             {/template}
-          `);
+          `
+          );
           assert.equal(result.diagnostics.length, 0);
         });
       }
 
       test(`error on strict equality`, () => {
-        const result = convertModule('test.ts', js`
+        const result = convertModule(
+          'test.ts',
+          js`
           import {html} from 'lit-html';
 
           /**
            * @soyCompatible
            */
           export const t = (a: string, b: string) => html\`\${a === b}\${a !== b}\`;
-        `);
+        `
+        );
         assert.equal(result.diagnostics.length, 2);
         assert.include(result.diagnostics[0].message, '=== is disallowed');
         assert.include(result.diagnostics[1].message, '!== is disallowed');
       });
 
       test('Array.length', () => {
-        const result = convertModule('test.ts', js`
+        const result = convertModule(
+          'test.ts',
+          js`
           import {html} from 'lit-html';
 
           /**
            * @soyCompatible
            */
           export const t = (a: string[]) => html\`\${a.length}\`;
-        `);
-        assert.equal(result.output, soy`
+        `
+        );
+        assert.equal(
+          result.output,
+          soy`
           {namespace test.ts}
 
           {template .t}
             {@param a: list<string>}
           {length($a)}
           {/template}
-        `);
+        `
+        );
         assert.equal(result.diagnostics.length, 0);
       });
 
       test('String.length', () => {
-        const result = convertModule('test.ts', js`
+        const result = convertModule(
+          'test.ts',
+          js`
           import {html} from 'lit-html';
 
           /**
            * @soyCompatible
            */
           export const t = (a: string) => html\`\${a.length}\`;
-        `);
-        assert.equal(result.output, soy`
+        `
+        );
+        assert.equal(
+          result.output,
+          soy`
           {namespace test.ts}
 
           {template .t}
             {@param a: string}
           {strLen($a)}
           {/template}
-        `);
+        `
+        );
         assert.equal(result.diagnostics.length, 0);
       });
-      
     });
-
   });
-
 });
-
 
 const convertModule = (fileName: string, source: string) => {
   const host = new TestHost({
     [fileName]: source,
   });
-  const program = ts.createProgram([fileName], {
-    target: ts.ScriptTarget.ES2017,
-    module: ts.ModuleKind.ESNext,
-    skipDefaultLibCheck: true,
-    skipLibCheck: true,
-  }, host);
-  const checker = program.getTypeChecker();   
+  const program = ts.createProgram(
+    [fileName],
+    {
+      target: ts.ScriptTarget.ES2017,
+      module: ts.ModuleKind.ESNext,
+      skipDefaultLibCheck: true,
+      skipLibCheck: true,
+    },
+    host
+  );
+  const checker = program.getTypeChecker();
   const sourceFile = program.getSourceFile(fileName)!;
   // TODO: assert 0 diagnostics for most tests
   // const diagnostics = program.getSemanticDiagnostics(sourceFile);
@@ -294,15 +370,21 @@ class TestHost implements ts.CompilerHost {
     this.files = new Map(Object.entries(files));
   }
 
-  resolveModuleNames(moduleNames: string[], _containingFile: string): (ts.ResolvedModule | undefined)[] {
+  resolveModuleNames(
+    moduleNames: string[],
+    _containingFile: string
+  ): (ts.ResolvedModule | undefined)[] {
     const resolvedNames = moduleNames.map((n) => {
       if (n === 'lit-html') {
-        const resolvedFileName = path.resolve(packageRoot, 'node_modules/lit-html/lit-html.d.ts');
+        const resolvedFileName = path.resolve(
+          packageRoot,
+          'node_modules/lit-html/lit-html.d.ts'
+        );
         return {
           resolvedFileName,
           isExternalLibraryImport: false,
         };
-      };
+      }
       return undefined;
     });
     return resolvedNames;
@@ -336,9 +418,19 @@ class TestHost implements ts.CompilerHost {
     return this.files.get(fileName);
   }
 
-  getSourceFile(fileName: string, _languageVersion: ts.ScriptTarget, onError?: (message: string) => void, _shouldCreateNewSourceFile?: boolean): ts.SourceFile | undefined {
+  getSourceFile(
+    fileName: string,
+    _languageVersion: ts.ScriptTarget,
+    onError?: (message: string) => void,
+    _shouldCreateNewSourceFile?: boolean
+  ): ts.SourceFile | undefined {
     if (this.fileExists(fileName)) {
-      return ts.createSourceFile(fileName, this.readFile(fileName)!, ts.ScriptTarget.ES2017, true);
+      return ts.createSourceFile(
+        fileName,
+        this.readFile(fileName)!,
+        ts.ScriptTarget.ES2017,
+        true
+      );
     } else if (onError !== undefined) {
       onError(`File not found ${fileName}`);
     }
